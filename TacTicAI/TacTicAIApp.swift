fe4713cd82506d3468c090c4fc59a96440d66020
Nursultan <nursultanyelemessov@gmail.com>
2023-09-24 15:01:04 +0600

    Initial Commit

---
+++ TacTicAI/TacTicAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TacTicAIApp.swift
+//  TacTicAI
+//
+//  Created by Nursultan Yelemessov on 24/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct TacTicAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
